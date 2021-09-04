f00068a5e6a102571611a7faa47ac4749cf92bc1
machiamavlad <59051338+machiamavlad@users.noreply.github.com>
2021-09-04 04:46:34 +0300

    added vrp lib & proxy / tunnel.

--- Essentials/fxmanifest.lua
+++ Essentials/fxmanifest.lua
@@ -1,12 +1,14 @@
 fx_version 'adamant'
 game 'gta5'
 author 'machiamavlad'
-description 'Cateva chestii simple dar necesare pentru orice server de FiveM de tip RolePlay.'
 
 client_scripts{
+	'@vrp/client/Proxy.lua',
+	'@vrp/client/Tunnel.lua',
 	'client/*.lua'
 }
 
-server_scripts{ 
-    "server/*.lua"
+server_scripts{
+	"@vrp/lib/utils.lua",
+	"server/*.lua"
 }
